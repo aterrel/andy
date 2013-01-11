3b98942b6f67aa0e3d14bf94a6cbd45e89936e03
Andy R. Terrel <andy.terrel@gmail.com>
2013-01-11 08:57:57 -0600

    Adding ufl

---
+++ ufl.dev.sh
@@ -0,0 +1,56 @@
+#/bin/bash
+
+PKG=ufl
+VERSION=dev
+ROOT_DIR=${WORK}/opt
+SRC_DIR=${ROOT_DIR}/src
+INSTALL_DIR=${ROOT_DIR}/apps/${PKG}/${VERSION}
+PKG_SRC_DIR=${SRC_DIR}/${PKG}
+LOGFILE=${SRC_DIR}/${PKG}-${VERSION}.log
+MODULE_DIR=${ROOT_DIR}/modulefiles/${PKG}
+MODULEFILE=${MODULE_DIR}/${VERSION}.lua
+
+# Update development direcotry or download it
+if [ -d ${PKG_SRC_DIR} ]; then
+    pushd ${PKG_SRC_DIR}
+    bzr pull
+    popd
+else 
+    bzr branch lp:${PKG}
+fi
+
+# Configure and make
+pushd ${PKG_SRC_DIR}
+module load python
+python setup.py install --prefix=${INSTALL_DIR} 2>&1 | tee ${LOGFILE}
+popd
+
+# Write modulefile
+mkdir -p ${MODULE_DIR}
+cat << EOF > ${MODULEFILE}
+-- -*- lua -*-
+help(
+[[
+This module loads the UFL python library
+It updates the PYTHONPATH, PATH, and MANPATH environment variable.
+
+Requires python module be loaded.
+]])     
+
+local version = "${VERSION}"
+local base    = "${INSTALL_DIR}"
+
+whatis("Description: UFL python library")
+whatis("URL: https://launchpad.net/ufl")
+
+prereq("python",)
+
+local pkg_path = pathJoin(base, "bin")
+local pkg_man = pathJoin(base, "share/man")
+local pkg_py = pathJoin(base, "lib/python2.7/site-packages")
+
+prepend_path("PATH", pkg_path)
+prepend_path("MANPATH", pkg_man)
+prepend_path("PYTHONPATH", pkg_py)
+
+EOF
