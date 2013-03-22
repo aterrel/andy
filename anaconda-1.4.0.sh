0026e629e7f4d3aabfaec836b59ce671cc2a7091
Andy R. Terrel <andy.terrel@gmail.com>
2013-03-22 15:27:06 -0500

    Adding anaconda build

---
+++ anaconda-1.4.0.sh
@@ -0,0 +1,62 @@
+#/bin/bash
+
+set -e
+
+function echo_and_run { 
+    echo "\$ $@" | tee -a $LOGFILE
+    "$@" 2>&1 | tee -a $LOGFILE
+    status=${PIPESTATUS[0]}
+    if [ $status -ne 0 ]; then
+        echo "error with $1"
+    fi
+    return $status
+}
+
+PKG=anaconda
+VERSION=1.4.0
+URL=https://store.continuum.io/cshop/anaconda
+DESCRIPTION="Python distribution for large-scale data processing"
+DOWNLOAD_URL=http://09c8d0b2229f813c1b93-c95ac804525aac4b6dba79b00b39d1d3.r79.cf1.rackcdn.com/Anaconda-1.4.0-Linux-x86_64.sh
+ROOT_DIR=${WORK}/opt
+SRC_DIR=${ROOT_DIR}/src
+INSTALL_DIR=${ROOT_DIR}/apps/${PKG}/${VERSION}
+BUILD_DIR=${PKG_SRC_DIR}/build/release
+TARBALL=${SRC_DIR}/Anaconda-${VERSION}-Linux-x86_64.sh
+LOGFILE=${SRC_DIR}/logs/${PKG}-${VERSION}.log
+MODULE_DIR=${ROOT_DIR}/modulefiles/${PKG}
+MODULEFILE=${MODULE_DIR}/${VERSION}.lua
+
+mkdir -p ${SRC_DIR}/logs
+echo "Starting build: `date`" | tee ${LOGFILE}
+# Remove previous source and unpack tarball
+if [ ! -f ${TARBALL} ]; then
+    echo_and_run wget ${DOWNLOAD_URL}
+fi
+echo_and_run rm -rf ${INSTALL_DIR}
+printf "\nyes\n${INSTALL_DIR}\n" | bash Anaconda-1.4.0-Linux-x86_64.sh | tee -a ${LOGFILE}
+test ${PIPESTATUS[0]}
+
+# Write modulefile
+echo_and_run mkdir -p ${MODULE_DIR}
+cat << EOF > ${MODULEFILE}
+-- -*- lua -*-
+help(
+[[
+This module loads the ${PKG} : ${DESCRIPTION}
+
+Updating PATH environment variables. Setting ${PKG^^}_DIR
+]])     
+
+local version = "${VERSION}"
+local base    = "${INSTALL_DIR}"
+
+whatis("Description: ${PKG}: ${DESCRIPTION}")
+whatis("URL: ${URL}")
+
+local pkg_bin = pathJoin(base, "bin")
+prepend_path("PATH", pkg_bin)
+
+setenv("${PKG^^}_DIR", base)
+EOF
+
+echo "Finishing build: `date`" | tee -a ${LOGFILE}
