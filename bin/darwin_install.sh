7fc27af09dabf4392dc83cee297ac2f1f69e7553
Andy R. Terrel <andy.terrel@gmail.com>
2014-11-13 07:08:17 -0600

    Teach Andy to check state on a few more commands

--- bin/darwin_install.sh
+++ bin/darwin_install.sh
@@ -3,5 +3,7 @@
 set -e
 set -x
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install tmux emacs
+if ! hash brew 2>/dev/null; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew install tmux emacs
+fi
